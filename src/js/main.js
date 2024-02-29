0fdef76382c680ae90fa1d32bb8bc84e5827dd5c
Serene <yiting536@gmail.com>
2024-02-29 22:11:57 +0800

    modify the function to simplify

--- src/js/main.js
+++ src/js/main.js
@@ -4,14 +4,19 @@ const days = document.getElementById("days");
 const previous = document.getElementById("previous");
 const next = document.getElementById("next");
 
-previous.addEventListener("click", showPreviousMonth);
-next.addEventListener("click", showNextMonth);
+previous.addEventListener("click", () => showMonth("Prev"));
+next.addEventListener("click", () => showMonth("Next"));
 
 //獲取年、月、日期、星期
 const now = new Date();
-let nowYear = now.getFullYear();
-let nowMonth = now.getMonth() + 1; //回傳為0-11
-const nowDate = now.getDate();
+const nowObj = {
+  year: now.getFullYear(),
+  month: now.getMonth() + 1,
+  day: now.getDate(),
+};
+// const nowYear = now.getFullYear();
+// const nowMonth = now.getMonth() + 1; //回傳為0-11
+// const nowDate = now.getDate();
 const weekdayList = [
   "Sunday",
   "Monday",
@@ -28,16 +33,16 @@ weekTitle.innerHTML = `<tr>${weekdayList
   .join("")}</tr>`;
 
 // 根據年份和月份顯示
-function showCalendar(nowYear, nowMonth) {
+function showCalendar(year = nowObj.year, month = nowObj.month) {
   // //讓月份能呈現兩位數
-  const nowMonthStr = nowMonth < 10 ? "0" + nowMonth : nowMonth;
-  yearAndMonth.innerText = `${nowYear} / ${nowMonthStr}`;
+  const nowMonthStr = month < 10 ? "0" + month : month;
+  yearAndMonth.innerText = `${year} / ${nowMonthStr}`;
 
   // // ${nowY}/${nowM}/1 是一個日期字串的模板，其中的 /1 表示該日期字串的日部分被設置為 1。
   // // 透過 .getDay() 方法獲取這一天是星期幾。
-  const firstDay = new Date(`${nowYear}/${nowMonthStr}/1`).getDay();
+  const firstDay = new Date(`${year}/${nowMonthStr}/1`).getDay();
   // 要得到月份有幾天，可以用`new Date(y, m, 0).getDate()`
-  const allDays = new Date(nowYear, nowMonth, 0).getDate();
+  const allDays = new Date(year, month, 0).getDate();
 
   const frontEmptyData = [];
   for (let i = 0; i < firstDay; i++) {
@@ -68,24 +73,60 @@ function showCalendar(nowYear, nowMonth) {
   days.innerHTML = display;
 }
 
-//上個月
-function showPreviousMonth() {
-  nowMonth -= 1;
-  if (nowMonth < 1) {
-    nowMonth = 12;
-    nowYear -= 1;
-  }
-  showCalendar(nowYear, nowMonth);
-}
+const showMonth = (method = "Prev") => {
+  const newMonth = method === "Prev" ? nowObj.month - 1 : nowObj.month + 1;
 
-//下個月
-function showNextMonth() {
-  nowMonth += 1;
-  if (nowMonth > 12) {
-    nowMonth = 1;
-    nowYear += 1;
-  }
-  showCalendar(nowYear, nowMonth);
-}
+  nowObj.month = newMonth;
 
-showCalendar(nowYear, nowMonth);
+  if (newMonth < 1) {
+    nowObj.year = nowObj.year - 1;
+    nowObj.month = 12;
+  }
+
+  if (newMonth > 12) {
+    nowObj.year = nowObj.year + 1;
+    nowObj.month = 1;
+  }
+
+  showCalendar(nowObj.year, nowObj.month);
+};
+
+// //上個月
+// function showPreviousMonth() {
+//   const newMonth = nowObj.month - 1;
+//   // nowMonth -= 1;
+//   if (newMonth < 1) {
+//     nowObj.year = nowObj.year - 1;
+//     nowObj.month = 12;
+
+//     showCalendar(nowObj.year, nowObj.month);
+
+//     return;
+//   }
+
+//   // nowObj.year = nowYear;
+//   nowObj.month = newMonth;
+
+//   showCalendar(nowObj.year, nowObj.month);
+// }
+
+// //下個月
+// function showNextMonth() {
+//   const newMonth = nowObj.month + 1;
+//   // nowMonth += 1;
+//   if (newMonth > 12) {
+//     nowObj.year = nowObj.year + 1;
+//     nowObj.month = 1;
+
+//     showCalendar(nowObj.year, nowObj.month);
+
+//     return;
+//   }
+
+//   // nowObj.year = nowYear;
+//   nowObj.month = newMonth;
+
+//   showCalendar(nowObj.year, nowObj.month);
+// }
+
+showCalendar();
